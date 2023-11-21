Fix shift in hessian, now works stably

examples/c2_tccsd_qicas.py
@@ -13,16 +13,16 @@ dmrgscf.settings.MPIPREFIX = ''
 
 
 n_core = 0                    # number of frozen orbitals
-ne = 14-2*n_core              # number of total electrons
+ne = 12-2*n_core              # number of total electrons
 n_cas = 8                     # number of active orbitals
 n_should_close = 2            # target number of closed orbitals
 n_act_e = ne-2*n_should_close # number of active electrons
 # r = [float(sys.argv[-2])]     # list of geometry parameters
 # bd = int(sys.argv[-1])        # max bond dimension for DMRG
-#r = np.arange(6.0,6.1,0.2)    # list of geometry parameters
-r =[2.0, 6.0]
+r = np.arange(3.2,6.0,0.2)    # list of geometry parameters
+r =[1.8, 2.8]
 
-bd = 200
+bd = 100
 tot_iter = 30
 E_casci = np.zeros((len(r), 3+tot_iter))      # array of output data
 E_tccsd = np.zeros((len(r), tot_iter))      # array of output data
@@ -37,7 +37,7 @@ for i in range(len(r)):
 
     # The following code runs a HF-CASSCF and HF-CASCI from sratch for comparison
     
-    mol = gto.M(atom='N 0 0 0; N 0 0 '+"{:.4}".format(r[i]), 
+    mol = gto.M(atom='C 0 0 0; C 0 0 '+"{:.4}".format(r[i]), 
         basis=basis,spin=0, verbose=1, 
         max_memory=50000,symmetry = False) # mem in MB
     mol.unit = 'B'
@@ -83,7 +83,7 @@ for i in range(len(r)):
     mf.verbose = 4
     mf.kernel()
     
-    mo_coeff = copy.deepcopy(mycas.mo_coeff)
+    mo_coeff = copy.deepcopy(mycasci.mo_coeff)
     no = len(mo_coeff)-n_core
 
     t0 = time.time()
@@ -96,11 +96,12 @@ for i in range(len(r)):
     act_space = (n_cas,ne-2*n_should_close)
    
     my_qicas = QICAS(mf=mf, mc=None, act_space=act_space) 
-    my_qicas.max_cycle = 60
+    my_qicas.max_cycle = 30
     my_qicas.max_M = bd
     my_qicas.step_size = 0.5
-    my_qicas.thresh = 1e-6
+    my_qicas.thresh = 1e-7
     my_qicas.casci_natorb = False
+    my_qicas.tcc_level_shift = 0.2
     my_qicas.dump_flags()
     
     # get the FCI wave function in the whole space
@@ -108,7 +109,16 @@ for i in range(len(r)):
 
 
     for micro_i in range(tot_iter):
-        print("mo_energy", mf.mo_energy)
+
+        if micro_i < 1:
+            my_qicas.tcc_max_cycle = 0
+            my_qicas.max_cycle = 5
+        else:
+            my_qicas.tcc_max_cycle = 50
+            my_qicas.max_cycle = 30
+        
+        my_qicas.step_size = np.max([my_qicas.step_size, 0.2])
+
         e_qicas = my_qicas.kernel(is_tcc=True,  inactive_indices=inactive_indices,
             mo_coeff=mo_coeff, method='nr')
         mo_coeff = my_qicas.mo_coeff.copy()
@@ -128,8 +138,6 @@ for i in range(len(r)):
         #tcc, t1, t2 = make_tailored_ccsd(tcc, mc)
         #tcc.verbose = mf.verbose
         #tcc.kernel()
-        #max_abs_t1 = np.max(np.abs(tcc.t1))
-        #print("max_abs_t1", max_abs_t1)
         
         # dump fcidump
         #fcidump.from_mo(mf.mol, 'fcidump', mo_coeff)
@@ -141,15 +149,16 @@ for i in range(len(r)):
         #dm2 = tcc.make_rdm2()
         #mo_coeff = make_no(dm1, mo_coeff)
 
-        #E_casci[i, micro_i+3] = mc.e_tot
         #E_tccsd[i, micro_i] = tcc.e_tot
+        E_tccsd[i, micro_i] = my_qicas.tcc_e_tot
+
 
         #gamma, Gamma = prep_rdm12(dm1,dm2)
 
+        entropy_tccsd[i, micro_i] = get_cost_fqi(my_qicas.gamma, my_qicas.Gamma, inactive_indices)
         #entropy_tccsd[i, micro_i] = get_cost_fqi(gamma, Gamma, inactive_indices)
-        #np.savetxt('n2_tccsd_natorb_energy_'+basis+'.6.0.txt',E_tccsd)
-        #np.savetxt("n2_qicas_tccsd_natorb_entropy_"+basis+".6.0.txt", np.asarray(entropy_tccsd))
-        #np.savetxt("n2_dmrg_tccsd_natorb_energy_"+basis+".6.0.txt", np.asarray(E_dmrg))
+        np.savetxt('c2_test_energy_'+basis+'.8in8.5.txt',E_tccsd)
+        np.savetxt("c2_test_entropy_"+basis+".8in8.5.txt", np.asarray(entropy_tccsd))
     t1 = time.time()
     print('icas time:',t1-t0)
 

examples/cr2_tccsd_qicas.py
@@ -13,22 +13,23 @@ dmrgscf.settings.MPIPREFIX = ''
 
 
 n_core = 0                    # number of frozen orbitals
-ne = 14-2*n_core              # number of total electrons
-n_cas = 8                     # number of active orbitals
-n_should_close = 2            # target number of closed orbitals
+ne = 24-2*n_core              # number of total electrons
+n_cas = 12                     # number of active orbitals
+n_should_close = 6            # target number of closed orbitals
 n_act_e = ne-2*n_should_close # number of active electrons
 # r = [float(sys.argv[-2])]     # list of geometry parameters
 # bd = int(sys.argv[-1])        # max bond dimension for DMRG
-#r = np.arange(6.0,6.1,0.2)    # list of geometry parameters
-r =[2.0, 6.0]
+r = np.arange(3.8,4.0,0.2)    # list of geometry parameters
+#r =[2.0]
+r = [2.4]
 
-bd = 200
-tot_iter = 30
+bd = 100
+tot_iter = 20
 E_casci = np.zeros((len(r), 3+tot_iter))      # array of output data
 E_tccsd = np.zeros((len(r), tot_iter))      # array of output data
 entropy_tccsd = np.zeros((len(r), tot_iter))      # array of output data
 E_dmrg = np.zeros((len(r), tot_iter))      # array of output data
-basis = 'ccpvdz'               # basis set
+basis = '631g'               # basis set
 
 
 for i in range(len(r)):
@@ -37,25 +38,25 @@ for i in range(len(r)):
 
     # The following code runs a HF-CASSCF and HF-CASCI from sratch for comparison
     
-    mol = gto.M(atom='N 0 0 0; N 0 0 '+"{:.4}".format(r[i]), 
+    mol = gto.M(atom='Cr 0 0 0; Cr 0 0 '+"{:.4}".format(r[i]), 
         basis=basis,spin=0, verbose=1, 
         max_memory=50000,symmetry = False) # mem in MB
-    mol.unit = 'B'
+    mol.unit = 'A'
     mol.build()
 
     mf = scf.RHF(mol)
     mf.kernel()
     
-    mycas = mcscf.CASSCF(mf,n_cas,ne-2*n_should_close)
-    mycas.verbose = 4
-    mycas.frozen = n_core
-    mycas.max_cycle_macro = 150
-    mycas.natorb = True
-    mycas.max_cycle_micro = 50
-    mycas.fix_spin_(ss=0)
-    etot = mycas.kernel()[0]
-    # casscf energy
-    E_casci[i,1] = etot
+    #mycas = mcscf.CASSCF(mf,n_cas,ne-2*n_should_close)
+    #mycas.verbose = 4
+    #mycas.frozen = n_core
+    #mycas.max_cycle_macro = 150
+    #mycas.natorb = True
+    #mycas.max_cycle_micro = 50
+    #mycas.fix_spin_(ss=0)
+    #etot = mycas.kernel()[0]
+    ## casscf energy
+    #E_casci[i,1] = etot
     
     
     
@@ -63,11 +64,17 @@ for i in range(len(r)):
     #    basis=basis,spin=0, verbose=1, 
     #    max_memory=50000,symmetry = False) # mem in MB
     #mol.unit = 'A'
-    mf = scf.RHF(mol)
-    mf.kernel()
+    #mf = scf.RHF(mol)
+    #mf.kernel()
     mycasci = mcscf.CASCI(mf,n_cas,ne-2*n_should_close)
     mycasci.verbose = 4
+    mycasci.natorb = True
+    shift = 20
+    mycasci.fcisolver.max_cycle = 400
+    mycasci.fix_spin_(ss=0, shift=shift)
     etot = mycasci.kernel(mf.mo_coeff)[0]
+
+   
     # casci energy
     E_casci[i,2] = etot
     # create molecule with desired geometry and basis
@@ -83,7 +90,7 @@ for i in range(len(r)):
     mf.verbose = 4
     mf.kernel()
     
-    mo_coeff = copy.deepcopy(mycas.mo_coeff)
+    mo_coeff = copy.deepcopy(mycasci.mo_coeff)
     no = len(mo_coeff)-n_core
 
     t0 = time.time()
@@ -96,11 +103,14 @@ for i in range(len(r)):
     act_space = (n_cas,ne-2*n_should_close)
    
     my_qicas = QICAS(mf=mf, mc=None, act_space=act_space) 
-    my_qicas.max_cycle = 60
+    my_qicas.max_cycle = 20
     my_qicas.max_M = bd
     my_qicas.step_size = 0.5
     my_qicas.thresh = 1e-6
     my_qicas.casci_natorb = False
+    my_qicas.casci_ss_shift = shift
+    my_qicas.casci_max_cycle = 400
+    my_qicas.tcc_level_shift = 0.5
     my_qicas.dump_flags()
     
     # get the FCI wave function in the whole space
@@ -108,7 +118,14 @@ for i in range(len(r)):
 
 
     for micro_i in range(tot_iter):
-        print("mo_energy", mf.mo_energy)
+
+        if micro_i < 1:
+            my_qicas.tcc_max_cycle = 0
+            my_qicas.max_cycle = 10
+        else:
+            my_qicas.tcc_max_cycle = 50
+            my_qicas.max_cycle = 30
+
         e_qicas = my_qicas.kernel(is_tcc=True,  inactive_indices=inactive_indices,
             mo_coeff=mo_coeff, method='nr')
         mo_coeff = my_qicas.mo_coeff.copy()
@@ -128,8 +145,6 @@ for i in range(len(r)):
         #tcc, t1, t2 = make_tailored_ccsd(tcc, mc)
         #tcc.verbose = mf.verbose
         #tcc.kernel()
-        #max_abs_t1 = np.max(np.abs(tcc.t1))
-        #print("max_abs_t1", max_abs_t1)
         
         # dump fcidump
         #fcidump.from_mo(mf.mol, 'fcidump', mo_coeff)
@@ -141,15 +156,17 @@ for i in range(len(r)):
         #dm2 = tcc.make_rdm2()
         #mo_coeff = make_no(dm1, mo_coeff)
 
-        #E_casci[i, micro_i+3] = mc.e_tot
         #E_tccsd[i, micro_i] = tcc.e_tot
+        E_tccsd[i, micro_i] = my_qicas.tcc_e_tot
+
 
         #gamma, Gamma = prep_rdm12(dm1,dm2)
 
+        entropy_tccsd[i, micro_i] = get_cost_fqi(my_qicas.gamma, my_qicas.Gamma, inactive_indices)
         #entropy_tccsd[i, micro_i] = get_cost_fqi(gamma, Gamma, inactive_indices)
-        #np.savetxt('n2_tccsd_natorb_energy_'+basis+'.6.0.txt',E_tccsd)
-        #np.savetxt("n2_qicas_tccsd_natorb_entropy_"+basis+".6.0.txt", np.asarray(entropy_tccsd))
-        #np.savetxt("n2_dmrg_tccsd_natorb_energy_"+basis+".6.0.txt", np.asarray(E_dmrg))
+        np.savetxt('cr2_tccsd_energy_'+basis+'.12in12.1.txt',E_tccsd)
+        np.savetxt("cr2_tccsd_entropy_"+basis+".12in12.1.txt", np.asarray(entropy_tccsd))
+        #np.savetxt("c2_dmrg_tccsd_qiorb_energy_"+basis+".scan.txt", np.asarray(E_dmrg))
     t1 = time.time()
     print('icas time:',t1-t0)
 

qicas.py
@@ -2,12 +2,12 @@ from functools import reduce
 import warnings
 import logging
 
-from pyscf import lib, dmrgscf, mcscf, cc, ci
+from pyscf import lib, dmrgscf, mcscf, cc, ci, symm
 import os
 import numpy as np
 from solver.jacobi import minimize_orb_corr_jacobi, reorder, reorder_fast, reorder_occ
 from solver.gradient import minimize_orb_corr_GD
-from tccsd import make_tailored_ccsd
+from tccsd import make_tailored_ccsd, make_no
 
 
 class QICAS:
@@ -43,8 +43,9 @@ class QICAS:
         # settings for tcc solver
         self.tcc_max_cycle = 100
         self.tcc_level_shift = 0.
-        self.tcc_casci_natorb = True
-        self.tcc_casci_max_cycle = 100
+        self.casci_natorb = True
+        self.casci_max_cycle = 300
+        self.casci_ss_shift = 0.5
         #
         self.verbose = mf.verbose
 
@@ -76,7 +77,7 @@ class QICAS:
         self.logger.info("tcc_e_tot = %s", str(self.tcc_e_tot))
         self.logger.info("tcc_max_cycle = %d", self.tcc_max_cycle)
         self.logger.info("tcc_level_shift = %f", self.tcc_level_shift)
-        self.logger.info("tcc_casci_natorb = %s", str(self.tcc_casci_natorb))
+        self.logger.info("tcc_casci_natorb = %s", str(self.casci_natorb))
 
 
     def kernel(self, inactive_indices, mo_coeff, is_tcc=False, method='newton-raphson'):
@@ -109,13 +110,16 @@ class QICAS:
             mc = mcscf.CASCI(self.mf, self.n_cas, self.n_act_e)
             #mc.frozen = self.n_core
             mc.verbose = self.verbose
-            mc.canonicalization = True 
-            mc.sorting_mo_energy = True
-            mc.max_cycle = self.tcc_casci_max_cycle
-            mc.fix_spin_(ss=0)
+            mc.canonicalization = True
+            mc.sorting_mo_energy = False 
+            mc.fcisolver.max_cycle = self.casci_max_cycle
+            mc.fix_spin_(ss=0, shift=self.casci_ss_shift)
             mc.tol = 1e-8
-            mc.natorb = self.tcc_casci_natorb
+            mc.natorb = self.casci_natorb
             mc.kernel(mo_coeff.copy())
+            #mc.mo_coeff, mc.ci, occ = mc.cas_natorb(sort=False)
+
+            
             tcc = cc.CCSD(self.mf, mo_coeff=mo_coeff.copy())
 
             self.logger.info('CASCI energy = %.6f',mc.e_tot)
@@ -129,6 +133,26 @@ class QICAS:
             self.logger.info('TCCSD energy = %.6f',tcc.e_tot)
             dm1 = tcc.make_rdm1()
             dm2 = tcc.make_rdm2()
+
+            # make NO in inactive space 
+            #n_core = (self.mf.mol.nelectron - self.n_act_e)//2
+            #inactive_subspace = list(range(n_core)) + list(range(self.n_cas+n_core,self.no))
+            #mo_coeff, _ = make_no(dm1, mo_coeff, inactive_subspace)
+
+            #tcc = cc.CCSD(self.mf, mo_coeff=mo_coeff.copy())
+
+            #self.logger.info('CASCI energy = %.6f',mc.e_tot)
+            #tcc, t1, t2 = make_tailored_ccsd(tcc, mc)
+            #tcc.verbose = self.verbose
+            #tcc.max_cycle = self.tcc_max_cycle
+            #tcc.level_shift = self.tcc_level_shift
+            #self.logger.info('mc mo energy = %s', str(mc.mo_energy))
+            #tcc.kernel()
+            #self.tcc_e_tot = tcc.e_tot
+            #self.logger.info('TCCSD energy = %.6f',tcc.e_tot)
+            #dm1 = tcc.make_rdm1()
+            #dm2 = tcc.make_rdm2()
+
             if self.debug:
                 assert np.isclose(np.sum(dm1.diagonal()), self.mf.mol.nelectron, atol=1e-6)
                 assert np.isclose(np.einsum('iijj->', dm2), self.mf.mol.nelectron*(self.mf.mol.nelectron-1), atol=1e-6)
@@ -169,17 +193,24 @@ class QICAS:
         V = reorder_occ(self.gamma.copy(),self.Gamma.copy(),self.n_cas)
         U_ = np.matmul(V,U)
         self.mo_coeff = mo_coeff @ U_.T
+        #self.mo_coeff = symm.symmetrize_orb(self.mf.mol, self.mo_coeff)
 
         # Post-QICAS CASCI block
-        mycas = mcscf.CASCI(self.mf,self.n_cas, self.n_act_e)
-        #mycas.frozen = self.n_core
-        mycas.fix_spin_(ss=0)
-        mycas.canonicalization = True
-        mycas.max_cycle = self.tcc_casci_max_cycle
-        mycas.natorb = self.tcc_casci_natorb
-        mycas.sorting_mo_energy = True
-        mycas.verbose = self.verbose
-        etot = mycas.kernel(self.mo_coeff)[0]
+        #mycas = mcscf.CASCI(self.mf,self.n_cas, self.n_act_e)
+        ##mycas.frozen = self.n_core
+        #mycas.fix_spin_(ss=0)
+        #mycas.canonicalization = True
+        #mycas.max_cycle = self.casci_max_cycle
+        #mycas.natorb = self.casci_natorb
+        #mycas.sorting_mo_energy = False 
+        #mycas.verbose = self.verbose
+        etot = mc.e_tot
+        #dm1 = mycas.make_rdm1()
+        ##dm2 = mycas.make_rdm2()
+        #gamma, Gamma = prep_rdm12(dm1,dm2)
+        #P = reorder_occ(gamma,Gamma,self.n_cas)
+        #self.mo_coeff = np.dot(self.mo_coeff,P)
+
 
 
         return etot
@@ -205,17 +236,17 @@ def dmrgci_prep(mc, mol, maxM, tol=1E-12):
     mc.fcisolver = dmrgscf.DMRGCI(mol, maxM=maxM, tol=tol)
     mc.fcisolver.runtimeDir = lib.param.TMPDIR
     mc.fcisolver.scratchDirectory = lib.param.TMPDIR
-    mc.fcisolver.threads = int(os.environ.get("OMP_NUM_THREADS", 4))
+    mc.fcisolver.threads = int(os.environ.get("OMP_NUM_THREADS", 8))
     mc.fcisolver.memory = int(mol.max_memory / 1000) # mem in GB
     mc.wfnsym='A1g'
     mc.canonicalization = False
     mc.natorb = False
     
     mc.fcisolver.restart = False
-    mc.fcisolver.scheduleSweeps =[50] 
-    mc.fcisolver.scheduleMaxMs = [maxM] 
-    mc.fcisolver.scheduleTols = [1e-6] 
-    mc.fcisolver.scheduleNoises = [0] 
+    mc.fcisolver.scheduleSweeps =[4, 4, 4, 4, 4] 
+    mc.fcisolver.scheduleMaxMs = [200, 400, 600, 800, 1000] 
+    mc.fcisolver.scheduleTols = [1e-6, 1e-6, 1e-7, 1e-7, 1e-7] 
+    mc.fcisolver.scheduleNoises = [1e-3, 1e-4, 1e-5, 1e-6, 0] 
     return mc
 
 
@@ -245,7 +276,19 @@ def prep_rdm12(dm1, dm2):
     return rdm1,rdm2
 
 
+def get_overlap(fcivector, cisdvec):
+    '''
+    Compute overlap between two vectors
 
+    Args:
+        fcivector (ndarray): FCI vector
+        cisdvec (ndarray): CISD vector
+
+    Returns:
+        overlap (float): overlap between fcivector and cisdvec
+    '''
+    overlap = np.dot(fcivector, cisdvec) / (np.linalg.norm(fcivector) * np.linalg.norm(cisdvec))
+    return overlap
 
 
 

solver/gradient.py
@@ -138,7 +138,7 @@ def FQI_display(gamma,Gamma,inactive_indices,verbose=True):
             
 
 def minimize_orb_corr_GD(gamma_,Gamma_,inactive_indices,step_size=0.1,thresh=1e-4,
-                         max_cycle=100, logger=None):
+                         max_cycle=100, noise=1e-3, logger=None):
     
     '''
 
@@ -151,6 +151,7 @@ def minimize_orb_corr_GD(gamma_,Gamma_,inactive_indices,step_size=0.1,thresh=1e-
         step_size (float): step size of gradient descent
         thresh (float): threshold of max gradient norm to stop optimization
         max_cycle (int): maximal number of cycles of jacobi rotation during orbital optimization
+        noise (float): noise added to the gradient to avoid trapping in local minima
         logger (logger): logger
 
     Returns:
@@ -167,35 +168,73 @@ def minimize_orb_corr_GD(gamma_,Gamma_,inactive_indices,step_size=0.1,thresh=1e-
     #X = (np.random.rand(no,no)/2-1)/1/(1+200*np.random.rand())
     X = np.zeros((no,no))
     X = X - X.T
-    U = expm(X)
-    U_ = np.kron(U,np.eye(2))
-    gamma0 = np.einsum('ia,jb,ab->ij',U_,U_,gamma_,optimize='optimal')
-    Gamma0 = np.einsum('ia,jb,kc,ld,abcd->ijkl',U,U,U,U,Gamma_,optimize='optimal')
-    #gamma0 = gamma_.copy()
-    #Gamma0 = Gamma_.copy()
+    U0 = expm(X)
+    #U_ = np.kron(U0,np.eye(2))
+    #gamma0 = np.einsum('ia,jb,ab->ij',U_,U_,gamma_,optimize='optimal')
+    #Gamma0 = np.einsum('ia,jb,kc,ld,abcd->ijkl',U0,U0,U0,U0,Gamma_,optimize='optimal')
+    gamma0 = gamma_.copy()
+    Gamma0 = Gamma_.copy()
 
-    U_tot = U
+    U_tot = U0
     grad = np.ones((no,no))
     
     n=0
-    while np.amax(abs(grad)) > thresh and n < max_cycle:
+    cost_old = get_cost_fqi(gamma0,Gamma0,inactive_indices)
+    delta_cost = np.inf
+    #while np.max(abs(grad)) > thresh and n < max_cycle:
+    level_shift_ave = 0.
+    level_shift = level_shift_ave
+    while np.abs(delta_cost) > thresh and n < max_cycle:
         n += 1
-        grad = FQI_grad(gamma0,Gamma0,inactive_indices)    
+        grad = FQI_grad(gamma0,Gamma0,inactive_indices)
+        #print("Max grad", np.max(abs(grad)))    
         hess = FQI_hess(gamma0,Gamma0,inactive_indices)
-        level_shift = 1e-2
-        X = -grad/(hess+level_shift*np.ones((no,no))) * step_size 
+        min_hess = np.min(np.abs(hess[np.abs(hess)>1e-8]))
+        print("min_hess", min_hess)
+        level_shift_ave += min_hess
+        #level_shift = (level_shift_ave/n)/10
+        level_shift = min_hess
+        #if min_hess > np.abs(level_shift):
+        #    level_shift = -min_hess
 
+        print("n", n, "level_shift", level_shift)
+        # print the sorted nonzero entries of hess
+        #min_hess = np.min(np.abs(hess[np.abs(hess)>1e-8]))
+        #level_shift = -np.min([level_shift, min_hess])
+
+        print("hess", np.sort(np.abs(hess[np.abs(hess)>1e-8]))[:10])
+        denom = hess-level_shift*np.ones((no,no))
+        X = -np.divide(grad, denom, out=np.zeros_like(grad), where=np.abs(denom)>min_hess) * step_size 
+        #else:
+        #    print("grad is small, using gradient descent")
+        #    X = -grad * step_size*0.1
+        # put entries smaller than abs(thresh) to zero
+        #cutoff = 1e-7
+        #tmp = X[np.abs(X)<cutoff]
+        #print("np.max(np.abs(tmp)) = ", np.max(np.abs(tmp)))
+        #X[np.abs(X)<cutoff] = 0
         
         U = expm(X)
         U_tot = np.matmul(U,U_tot)
         U_ = np.kron(U,np.eye(2))
-        gamma0 = np.einsum('ia,jb,ab->ij',U_,U_,gamma0,optimize='optimal')
-        Gamma0 = np.einsum('ia,jb,kc,ld,abcd->ijkl',U,U,U,U,Gamma0,optimize='optimal')
-        cost = get_cost_fqi(gamma0,Gamma0,inactive_indices)
+        gamma0_ = np.einsum('ia,jb,ab->ij',U_,U_,gamma0,optimize='optimal')
+        Gamma0_ = np.einsum('ia,jb,kc,ld,abcd->ijkl',U,U,U,U,Gamma0,optimize='optimal')
+        cost = get_cost_fqi(gamma0_,Gamma0_,inactive_indices)
+        delta_cost = cost_old - cost
+        #if delta_cost > 0:
+            #print("cost increased, using previous orbitals")
+            #level_shift += 0.9
+            #step_size *= 0.95
+        print("step_size", step_size)  
+        
+        gamma0 = gamma0_.copy()
+        Gamma0 = Gamma0_.copy()
+
+        cost_old = cost
         if n % 1 == 0:
             if logger is not None:
-                logger.info("iteration:"+ str(n) + " max |grad| = "+str(np.amax(abs(grad))) + " cost = " +str(cost))
+                logger.info("iteration:"+ str(n) + " max |grad| = "+str(np.max(abs(grad))) + " cost = " +str(cost))
             else:
-                print("iteration", n, "max grad", np.amax(abs(grad)), "cost", cost)
+                print("iteration", n, "max grad", np.max(abs(grad)), "cost", cost)
 
     return U_tot, gamma0, Gamma0

tccsd.py
@@ -47,22 +47,27 @@ def make_tailored_ccsd(cc, cas):
         ref_det_idx = np.argmax(np.abs(cisdvec))
         curr_str = '1'*cc.nocc*2
         det_str = pyscf.fci.cistring.addr2str(cc.nmo, cc.nocc*2, ref_det_idx)
-        det_bin = bin(det_str)[2:]
+        det_bin = bin(ref_det_idx)[2:]
         locs = list(set(find_ones(det_bin)).symmetric_difference(set(find_ones(curr_str))))
-        print("Swapping indices in determinant: ", locs//2)
-        if len(locs) > 0:
-            if len(locs) > 2:
-                raise ValueError("Error: more than two orbitals to swap. This is not supported yet.")
-                
-            locs = np.array(locs)//2
+        act_dm1 = cas.fcisolver.make_rdm1(cas.ci, cas.ncas, cas.nelecas).diagonal()
+        sort_idx = list(range(cas.ncore)) + list(np.argsort(act_dm1)[::-1] + cas.ncore) + list(range(cas.ncore+cas.ncas, cc.nmo))
+        # sort the orbitals
+        cas.mo_coeff = cas.mo_coeff[:,sort_idx]
 
-            # swap the orbitals
-            mo_coeff = cas.mo_coeff.copy()
-            cas.mo_coeff[:,locs] = mo_coeff[:,[locs[1],locs[0]]]
-            #ucas = cas.mo_coeff[:,cas.ncore:cas.ncore+cas.ncas].copy()
+        #if len(locs) > 0:
+        #    locs = np.array(locs)//2
+        #    if np.min(locs) > cas.ncore and np.max(locs) < cas.ncore+cas.ncas:
+        #        print("Swapping indices in determinant: ", locs)
+        #        if len(locs) > 2:
+        #            raise ValueError("Error: more than two orbitals to swap. This is not supported yet.")
 
-            # update the ci vectors
-            #cas.ci = cas.fcisolver.transform_ci_for_orbital_rotation(cas.ci, cas.ncas, cas.nelecas, ucas)
+        #        # swap the orbitals
+        #        mo_coeff = cas.mo_coeff.copy()
+        #        cas.mo_coeff[:,locs] = mo_coeff[:,[locs[1],locs[0]]]
+                #ucas = cas.mo_coeff[:,cas.ncore:cas.ncore+cas.ncas].copy()
+
+                # update the ci vectors
+                #cas.ci = cas.fcisolver.transform_ci_for_orbital_rotation(cas.ci, cas.ncas, cas.nelecas, ucas)
         return cas
 
     def find_ones(s):
@@ -74,17 +79,19 @@ def make_tailored_ccsd(cc, cas):
         c0, c1, c2 = pyscf.ci.cisd.cisdvec_to_amplitudes(cisdvec, cas.ncas, nocc_cas)
         c1_max = np.max(np.abs(c1))
         print("|C0| = %.4e, |C1_max| = %.4e" % (np.abs(c0), c1_max))
-        if np.abs(c0) < c1_max:
-            print("Warning: |C0| = %.4e is smaller than |C1_max| = %.4e. Current choice of reference determinant is bad!" % (np.abs(c0), c1_max))
-            print("Trying to find a better reference determinant in the singles space...")
-            cas = find_ref_det(cas)
-            print("Rerun CASCI to get new CI vector")
-            cas.kernel()
-            cisdvec = pyscf.ci.cisd.from_fcivec(cas.ci, cas.ncas, nelec_cas)
-            c0, c1, c2 = pyscf.ci.cisd.cisdvec_to_amplitudes(cisdvec, cas.ncas, nocc_cas)
-            c1_max = np.max(np.abs(c1))
-            print("New CI coeffs:")
-            print("|C0| = %.4e, |C1_max| = %.4e" % (np.abs(c0), c1_max))
+        #while np.abs(c0) < c1_max or abs(c0) < 2e-3:
+        #    print("Warning: |C0| = %.4e is smaller than |C1_max| = %.4e. Current choice of reference determinant is bad!" % (np.abs(c0), c1_max))
+        #    print("Trying to find a better reference determinant in the singles space...")
+        #    cas = find_ref_det(cas)
+        #    print("Rerun CASCI to get new CI vector")
+        #    cas.natorb = True
+        #    cas.kernel()
+        #    cas.natorb = False 
+        #    cisdvec = pyscf.ci.cisd.from_fcivec(cas.ci, cas.ncas, nelec_cas)
+        #    c0, c1, c2 = pyscf.ci.cisd.cisdvec_to_amplitudes(cisdvec, cas.ncas, nocc_cas)
+        #    c1_max = np.max(np.abs(c1))
+        #    print("New CI coeffs:")
+        #    print("|C0| = %.4e, |C1_max| = %.4e" % (np.abs(c0), c1_max))
 
         assert (abs(c0) > 1e-8)
         if (abs(c0) < 2e-3):
@@ -150,23 +157,39 @@ def make_tailored_ccsd(cc, cas):
     cc.make_rdm2 = make_rdm2
     return cc, t1_init, t2_init 
 
-def make_no(rdm1, mo_coeff):
+def make_no(rdm1, mo_coeff, subspace=None):
     """
     Make natural orbitals from 1-RDM.
     
     Args:
-        rdm1: 1-RDM
-        mo_coeff: MO coefficients
+        rdm1 (ndarray): 1-RDM
+        mo_coeff (ndarray): MO coefficients
+        subspace (list): space indices in which natural orbitals are constructed
     
     Returns:
         no_coeff: Natural orbitals
     """
+    if subspace is not None:
+        rdm1_sub = rdm1[subspace,:][:,subspace]
+        mo_coeff_sub = mo_coeff[:, subspace]
+    else:
+        rdm1_sub = rdm1
+        mo_coeff_sub = mo_coeff
     # diagonalize 1-RDM
-    e, v = np.linalg.eigh(rdm1)
+    e, v = np.linalg.eigh(rdm1_sub)
     # sort in descending order
     idx = np.argsort(e)[::-1]
     e = e[idx]
     v = v[:,idx]
     # transform to NO basis
-    no_coeff = np.dot(mo_coeff, v)
-    return no_coeff
+    no_coeff_sub = np.dot(mo_coeff_sub, v)
+    no_coeff = mo_coeff.copy()
+    if subspace is not None:
+        no_coeff[:, subspace] = no_coeff_sub
+        rdm1_new = rdm1.copy()
+        rdm1_new[subspace,:][:,subspace] = np.diag(np.ones(len(subspace))*2)
+    else:
+        no_coeff = no_coeff_sub
+        rdm1_new = np.diag(np.ones(len(rdm1))*2)
+    
+    return no_coeff, rdm1_new